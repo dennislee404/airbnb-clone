c8cbbe4dfb37ce35c001b1c905f60d0056ee2a89
Dennis Lee <dennislee404@gmail.com>
2023-10-10 12:01:12 +0800

    add new listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,16 +1,25 @@
 class ListingsController < ApplicationController
 	def index
+		@listings = current_user.listings.all
 	end
 
 	def new
+		@listing = Listing.new
 	end
 
 	def create
+		@listing = current_user.listings.create(listing_params)
+
+		if @listing.save
+			redirect_to listing_path(@listing)
+		else 
+			render :new 
+		end		
 	end
 
 	def show
 	end
-	
+
 	def edit
 	end
 
@@ -19,4 +28,9 @@ class ListingsController < ApplicationController
 
 	def destroy
 	end
+
+	private
+		def listing_param
+			# params.require(:listing).permit(:)
+		end
 end
